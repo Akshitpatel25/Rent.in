a37311dbfad89501b0e0ff514b9b189e846e6a2b
Akshitpatel25 <patelakshit1225@gmail.com>
2025-01-06 20:39:57 +0530

    improve left navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,51 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import axios from "axios";
-import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import axios from 'axios';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Navbar({ Nav_user }: any) {
   const [isopenHamb, setIsopenHamb] = useState(false);
   const [isopenUserProfile, setIsopenUserProfile] = useState(false);
   const [isHydrated, setIsHydrated] = useState(false);
-  const [isHamopen, setIsHamopen] = useState(false);
+const[isHamopen, setIsHamopen] = useState(false);
   const router = useRouter();
 
+  // Ensure hydration before rendering
   useEffect(() => {
     setIsHydrated(true);
-    if (isopenHamb) {
+    if(isopenHamb) {
         setTimeout(() => {
-            setIsHamopen((prev) => !prev);
+            setIsHamopen(true);
         },400);
-    }
-    else{
-        setIsHamopen((prev)=> !prev);
+    }else {
+        setIsHamopen(false);
     }
   }, [isopenHamb]);
 
+  if (!isHydrated) return null;
 
   // Functions
   async function logout() {
     try {
-      await axios.get("/api/logout");
+      await axios.get('/api/logout');
       signOut();
-      router.push("/login");
+      router.push('/login');
     } catch (error) {
-      console.error("Logout failed:", error);
+      console.error('Logout failed:', error);
     }
   }
 
   return (
     <>
       {/* Navbar */}
-      <div className="w-full h-full pl-2 pr-2 border border-black border-opacity-10 flex justify-between items-center">
+      <div className="w-full h-full pl-2 pr-2 border border-opacity-15 border-black flex justify-between items-center">
         {/* Hamburger Bar */}
         <div
           className={`hamberger-bar p-2 cursor-pointer flex flex-col items-center transition-all duration-300 ${
-            isopenHamb ? "gap-0" : "gap-2"
+            isopenHamb ? 'gap-0' : 'gap-2'
           }`}
           onClick={() => setIsopenHamb(!isopenHamb)}
         >
@@ -61,75 +62,34 @@ export default function Navbar({ Nav_user }: any) {
           className="cursor-pointer w-10 h-10 md:w-14 md:h-14 rounded-full flex justify-center items-center"
           onClick={() => setIsopenUserProfile(!isopenUserProfile)}
         >
-          <Image
-            src="/userIcon1.png"
-            alt="Google Sign-In"
-            width={50}
-            height={50}
-          />
+          <Image src="/userIcon1.png" alt="Google Sign-In" width={50} height={50} />
         </div>
       </div>
 
       {/* Left Navigation Bar */}
       <div
         className={`absolute top-0 left-0 bg-white bg-opacity-15 backdrop-blur-md h-full z-10 flex flex-col justify-start items-start border border-[#283618] border-opacity-20 transition-all duration-500 ${
-          isopenHamb ? "p-3 w-72" : "p-0 w-0"
+          isopenHamb ? 'p-3 w-72' : 'p-0 w-0'
         }`}
       >
-        <h1
-          className="text-2xl w-full flex justify-end cursor-pointer"
-          onClick={() => setIsopenHamb(!isopenHamb)}
-        >
+        <h1 className="text-2xl w-full flex justify-end cursor-pointer" onClick={() => setIsopenHamb(!isopenHamb)}>
           X
         </h1>
 
         {/* Navigation Details */}
         {isHamopen && (
           <>
-            <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-4 ">
-              Welcome!
-              <br />
-              <span className="font-bold">{Nav_user}</span>
-            </h1>
+            <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-4">Welcome!<br /><span className="font-bold">{Nav_user}</span></h1>
             <nav className="w-full flex flex-col gap-y-2">
-              <Link
-                href="/dashboard"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="/create-new-rent"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                Create New Rent
-              </Link>
-              <Link
-                href="/all-properties"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                All Properties
-              </Link>
-              <Link
-                href="/add-maintanence"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                Add Maintenance
-              </Link>
-              <Link
-                href="/add-expense"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                Add Expense
-              </Link>
-              <Link
-                href="/revenue"
-                className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-70 transition-all ease-in-out"
-              >
-                Revenue
-              </Link>
+            <Link href="/dashboard" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">Dashboard</Link>
+            <Link href="/user-profile" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">User Profile</Link>
+              <Link href="/create-new-rent" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">Create New Rent</Link>
+              <Link href="/all-properties" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">All Properties</Link>
+              <Link href="/add-maintanence" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">Add Maintenance</Link>
+              <Link href="/add-expense" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">Add Expense</Link>
+              <Link href="/revenue" className="p-2 bg-white bg-opacity-40 rounded-md text-center md:text-xl hover:bg-opacity-80">Revenue</Link>
               <div
-                className="p-2 bg-black text-white bg-opacity-60 rounded-md text-center md:text-xl cursor-pointer hover:bg-opacity-70 transition-all ease-in-out"
+                className="p-2 bg-black text-white bg-opacity-60 rounded-md text-center md:text-xl cursor-pointer"
                 onClick={logout}
               >
                 Logout
@@ -141,37 +101,15 @@ export default function Navbar({ Nav_user }: any) {
 
       {/* User Profile Dropdown */}
       <div
-        className="absolute top-[70px] right-0 md:top-[90px] lg:top-[100px] xl:top-[70px] border w-40 bg-white bg-opacity-15 rounded-md backdrop-blur-sm z-10 transition-all ease-in-out duration-500 border-[#283618] border-opacity-20 flex flex-col gap-y-2"
-        style={{
-          width: `${isopenUserProfile ? "180px" : "0px"}`,
-          height: `${isopenUserProfile ? "120px" : "0px"}`,
-          padding: `${isopenUserProfile ? "7px" : "0px"}`,
-        }}
+        className="absolute top-[70px] right-1 w-48 h-28  md:top-[90px] lg:top-[100px] xl:top-[70px] p-2 border bg-white bg-opacity-30 rounded-md backdrop-blur-sm z-10 transition-all ease-in-out duration-500 border-[#283618] border-opacity-20 flex flex-col gap-y-2"
+        style={{ display: `${isopenUserProfile ? 'flex' : 'none'}` }}
       >
-        <div className="w-full h-1/2 bg-[#DDA15E] rounded-md bg-opacity-50 flex justify-center items-center cursor-pointer">
-          <div
-            className="transition-all ease-in-out delay-500 duration-500"
-            style={{ fontSize: `${isopenUserProfile ? "16px" : "0px"}` }}
-          >
-            User Profile
-          </div>
+        <div className="w-full h-1/2 bg-[#DDA15E] hover:bg-opacity-80 rounded-md bg-opacity-50 flex justify-center items-center cursor-pointer">
+          <div className="transition-all ease-in-out  duration-500" style={{ display: `${isopenUserProfile ? 'flex' : 'none'}`, fontSize: `${isopenUserProfile ? '16px' : '0px'}`  }}>User Profile</div>
         </div>
-        <div
-          className="w-full h-1/2 bg-black rounded-md flex justify-center items-center cursor-pointer"
-          onClick={logout}
-        >
-          <Image
-            src="/logoutIconWhite.png"
-            width={15}
-            height={15}
-            alt="Logout"
-          />
-          <div
-            className="transition-all ease-in-out duration-500 text-white"
-            style={{ fontSize: `${isopenUserProfile ? "16px" : "0px"}` }}
-          >
-            Logout
-          </div>
+        <div className="w-full h-1/2 bg-black rounded-md flex justify-center items-center cursor-pointer" onClick={logout}>
+          <Image src="/logoutIconWhite.png" width={20} height={20} alt="Logout" />
+          <div className="transition-all ease-in-out duration-500 text-white" style={{ fontSize: `${isopenUserProfile ? '16px' : '0px'}` }}>Logout</div>
         </div>
       </div>
     </>

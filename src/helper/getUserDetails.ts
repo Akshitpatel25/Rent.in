0c72f5bebf25fca448ce117fd8b00e2ec2bf197c
Akshitpatel25 <patelakshit1225@gmail.com>
2025-02-06 09:57:25 +0530

    retive previous code

--- src/helper/getUserDetails.ts
+++ src/helper/getUserDetails.ts
@@ -6,28 +6,38 @@ import {auth} from "@/auth"
 import axios from "axios";
 
 const getUserByCookies = async (request: NextRequest) => {
+
     try {
         await dbConnect();
+        const session = await auth();
         const token = request.cookies.get('Rtoken')?.value || '';
-        console.log("Rtoken from request:", token);  // Log received cookie
+        console.log("Rtoken:",token);
+        
+        
+        if (token.length === 0) {
+            const apiUrl = process.env.DOMAIN || "http://localhost:3000";
+            await axios.post(`${apiUrl}/api/googleSignin`, { name: session?.user?.name, email: session?.user?.email });
+            const user = await User.findOne({email: session?.user?.email});
+            return user?._id;
+        }else {
 
-        if (!token) {
-            console.log("No token found in cookies");
-            return null;
+            const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET!) as {
+                id: string;
+                email: string;
+            };
+            console.log("Decoded Token:",decodedToken);
+            
+            return decodedToken.id;
         }
 
-        const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET!) as {
-            id: string;
-            email: string;
-        };
-
-        console.log("Decoded Token:", decodedToken);
-        return decodedToken.id;
-    } catch (error) {
-        console.log("getUserByCookies failed:", error);
+    } catch (error:any) {
+        console.log("getUserByCookies failed: ", error);
         return null;
+        
     }
-};
 
+    
+
+}
 
 export {getUserByCookies};
